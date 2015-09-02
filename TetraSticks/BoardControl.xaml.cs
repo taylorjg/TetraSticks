813d8989c31e49bb67bb5b5ac6a58a807810d928
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-09-02 18:42:47 +0100

    Got a bit of basic line drawing working

--- TetraSticks/BoardControl.xaml.cs
+++ TetraSticks/BoardControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -18,6 +20,76 @@ namespace TetraSticks
         public void DrawGrid()
         {
             DrawGridLines();
+        }
+
+        // PointCollection pts ???
+        public void DrawLine(params Point[] pts)
+        {
+            var aw = ActualWidth;
+            var ah = ActualHeight;
+            var sw = (aw - GridLineThickness) / 5;
+            var sh = (ah - GridLineThickness) / 5;
+
+            var transformedPts = pts.Select(pt => new Point(pt.X * sw + GridLineHalfThickness, (5 - pt.Y) * sh + GridLineHalfThickness)).ToList();
+            var polyLineSegment = new PolyLineSegment(transformedPts, true);
+            var pathFigure = new PathFigure { StartPoint = transformedPts.First() };
+            pathFigure.Segments.Add(polyLineSegment);
+            var pathGeometry = new PathGeometry();
+            pathGeometry.Figures.Add(pathFigure);
+            var path = new Path
+            {
+                Stroke = new SolidColorBrush(Colors.BlueViolet),
+                StrokeThickness = GridLineThickness * 2,
+                StrokeStartLineCap = PenLineCap.Round,
+                StrokeEndLineCap = PenLineCap.Round,
+                StrokeLineJoin = PenLineJoin.Round,
+                Data = pathGeometry
+            };
+            BoardCanvas.Children.Add(path);
+        }
+
+        public void DrawSegmentHorizontal(Point pt)
+        {
+            var aw = ActualWidth;
+            var ah = ActualHeight;
+            var sw = (aw - GridLineThickness) / 5;
+            var sh = (ah - GridLineThickness) / 5;
+
+            var segment = new Line
+            {
+                Stroke = new SolidColorBrush(Colors.BlueViolet),
+                StrokeThickness = GridLineThickness*2,
+                X1 = 0,
+                Y1 = 0,
+                X2 = sw,
+                Y2 = 0
+            };
+            Canvas.SetLeft(segment, pt.X * sw + GridLineHalfThickness);
+            Canvas.SetBottom(segment, pt.Y * sh - GridLineHalfThickness);
+            BoardCanvas.Children.Add(segment);
+            //_pieceDetails[rotatedPiece.Piece.Name] = Tuple.Create(rotatedPiece.Orientation, x, y, pieceControl);
+        }
+
+        public void DrawSegmentVertical(Point pt)
+        {
+            var aw = ActualWidth;
+            var ah = ActualHeight;
+            var sw = (aw - GridLineThickness) / 5;
+            var sh = (ah - GridLineThickness) / 5;
+
+            var segment = new Line
+            {
+                Stroke = new SolidColorBrush(Colors.BlueViolet),
+                StrokeThickness = GridLineThickness*2,
+                X1 = 0,
+                Y1 = 0,
+                X2 = 0,
+                Y2 = sh
+            };
+            Canvas.SetLeft(segment, pt.X * sw + GridLineHalfThickness);
+            Canvas.SetBottom(segment, pt.Y * sh + GridLineHalfThickness);
+            BoardCanvas.Children.Add(segment);
+            //_pieceDetails[rotatedPiece.Piece.Name] = Tuple.Create(rotatedPiece.Orientation, x, y, pieceControl);
         }
 
         private void DrawGridLines()
--- TetraSticks/MainWindow.xaml.cs
+++ TetraSticks/MainWindow.xaml.cs
@@ -7,7 +7,25 @@ namespace TetraSticks
         public MainWindow()
         {
             InitializeComponent();
-            ContentRendered += (_, __) => BoardControl.DrawGrid();
+            ContentRendered += (_, __) =>
+            {
+                BoardControl.DrawGrid();
+
+                BoardControl.DrawSegmentHorizontal(new Point(3, 3));
+
+                BoardControl.DrawSegmentVertical(new Point(4, 1));
+
+                BoardControl.DrawLine(
+                    new Point(0, 0),
+                    new Point(1, 0),
+                    new Point(1, 1),
+                    new Point(2, 1));
+
+                BoardControl.DrawLine(new Point(1, 4), new Point(1, 5));
+                BoardControl.DrawLine(new Point(1, 4), new Point(1, 3));
+                BoardControl.DrawLine(new Point(1, 4), new Point(0, 4));
+                BoardControl.DrawLine(new Point(1, 4), new Point(2, 4));
+            };
         }
     }
 }
