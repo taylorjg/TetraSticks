388db6de2e353b0df1de5d367a2179fa31e62ece
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-09-11 18:49:19 +0100

    More work on better drawing

--- TetraSticks/View/BoardControl.xaml.cs
+++ TetraSticks/View/BoardControl.xaml.cs
@@ -37,13 +37,6 @@ namespace TetraSticks.View
             Down
         };
 
-        private enum Disposition
-        {
-            First,
-            Middle,
-            Last
-        }
-
         public BoardControl()
         {
             InitializeComponent();
@@ -124,22 +117,41 @@ namespace TetraSticks.View
             var combinedLineSegments = CombineConsecutiveLineSegments(lineSegments);
             var segments = BuildPathSegmentCollection(combinedLineSegments);
             var sortedSegments = SortPathSegmentCollection(segments);
+
             return new PathGeometry
             {
                 Figures = new PathFigureCollection
                 {
                     new PathFigure
                     {
-                        StartPoint = new Point(0, 0), // TODO: use proper value here...
+                        StartPoint = GetStartingPoint(combinedLineSegments),
                         Segments = sortedSegments
                     }
                 }
             };
         }
 
-        private static IImmutableList<Tuple<Coords, Coords, Direction, Disposition>> LineToLineSegments(IImmutableList<Coords> coords)
+        private Point GetStartingPoint(IEnumerable<Tuple<Coords, Coords, Direction>> line)
         {
-            Func<Coords, Coords, Direction> f1 = (c1, c2) =>
+            var first = line.First();
+            switch (first.Item3)
+            {
+                case Direction.Left:
+                    return CoordsToInsetLowerRightHorizontal(first.Item1);
+                case Direction.Right:
+                    return CoordsToInsetUpperLeftHorizontal(first.Item1);
+                //case Direction.Up:
+                //    return CoordsToInsetLowerLeftVertical(first.Item1);
+                //case Direction.Down:
+                //    return CoordsToInsetUpperRightVertical(first.Item1);
+                default:
+                    throw new InvalidOperationException("...");
+            }
+        }
+
+        private static IImmutableList<Tuple<Coords, Coords, Direction>> LineToLineSegments(IImmutableList<Coords> coords)
+        {
+            Func<Coords, Coords, Direction> calculateDirection = (c1, c2) =>
             {
                 if (c1.Y == c2.Y && c1.X > c2.X) return Direction.Left;
                 if (c1.Y == c2.Y && c1.X < c2.X) return Direction.Right;
@@ -148,96 +160,170 @@ namespace TetraSticks.View
                 throw new InvalidOperationException("...");
             };
 
-            Func<int, Disposition> f2 = i =>
-            {
-                if (i == 1) return Disposition.First;
-                if (i == coords.Count - 1) return Disposition.Last;
-                return Disposition.Middle;
-            };
+            var result = new List<Tuple<Coords, Coords, Direction>>();
 
-            var result = new List<Tuple<Coords, Coords, Direction, Disposition>>();
-
+            // Can this be done using Aggregate ?
             for (var i = 1; i < coords.Count; i++)
             {
                 var coords1 = coords[i - 1];
                 var coords2 = coords[i];
-                var direction = f1(coords1, coords2);
-                var disposition = f2(i);
-                result.Add(Tuple.Create(coords1, coords2, direction, disposition));
+                var direction = calculateDirection(coords1, coords2);
+                result.Add(Tuple.Create(coords1, coords2, direction));
             }
 
             return result.ToImmutableList();
         }
 
-        private static IImmutableList<Tuple<Coords, Coords, Direction, Disposition>> CombineConsecutiveLineSegments(IImmutableList<Tuple<Coords, Coords, Direction, Disposition>> lineSegments)
+        private static IImmutableList<Tuple<Coords, Coords, Direction>> CombineConsecutiveLineSegments(IImmutableList<Tuple<Coords, Coords, Direction>> lineSegments)
         {
-            var result = new List<Tuple<Coords, Coords, Direction, Disposition>>();
+            var result = new List<Tuple<Coords, Coords, Direction>>();
 
-            var currentLineSegments = new List<Tuple<Coords, Coords, Direction, Disposition>> {lineSegments[0]};
+            var currentLineSegments = new List<Tuple<Coords, Coords, Direction>> {lineSegments[0]};
+
+            // Can this be done using Aggregate ?
             for (var i = 1; i < lineSegments.Count; i++)
             {
                 var nextLineSegment = lineSegments[i];
+
                 if (nextLineSegment.Item3 == currentLineSegments[0].Item3)
                 {
                     currentLineSegments.Add(nextLineSegment);
                 }
                 else
                 {
+                    // TODO: Extract this duplicated code into a local Func ?
                     var combinedLineSegment = Tuple.Create(
                         currentLineSegments.First().Item1,
                         currentLineSegments.Last().Item2,
-                        currentLineSegments.First().Item3,
-                        Disposition.Middle);
+                        currentLineSegments.First().Item3);
                     result.Add(combinedLineSegment);
-                    currentLineSegments = new List<Tuple<Coords, Coords, Direction, Disposition>> {nextLineSegment};
+
+                    currentLineSegments = new List<Tuple<Coords, Coords, Direction>> {nextLineSegment};
                 }
             }
 
             if (currentLineSegments.Any())
             {
+                // TODO: Extract this duplicated code into a local Func ?
                 var combinedLineSegment = Tuple.Create(
                     currentLineSegments.First().Item1,
                     currentLineSegments.Last().Item2,
-                    currentLineSegments.First().Item3,
-                    Disposition.Middle);
+                    currentLineSegments.First().Item3);
                 result.Add(combinedLineSegment);
             }
-
-            result[0] = Tuple.Create(result.First().Item1, result.First().Item2, result.First().Item3, Disposition.First);
-            result[result.Count - 1] = Tuple.Create(result.Last().Item1, result.Last().Item2, result.Last().Item3, Disposition.Last);
-
-            // What about case of I where there will be only 1 combined segment ?
-            // Disposition.First and Disposition.Last ???
-            // I guess we could make Disposition a bit flags enum ?
-            // Or add a new enum value FirstAndLast ?
-            // Or could we get away with simply Middle/End ?
 
             return result.ToImmutableList();
         }
 
-        private PathSegmentCollection BuildPathSegmentCollection(IImmutableList<Tuple<Coords, Coords, Direction, Disposition>> lineSegments)
+        private PathSegmentCollection BuildPathSegmentCollection(IImmutableList<Tuple<Coords, Coords, Direction>> lineSegments)
         {
-            return null;
+            var segments = new PathSegmentCollection();
+            for (var i = 0; i < lineSegments.Count; i++)
+            {
+                var lineSegment = lineSegments[i];
+                var isFirst = i == 0;
+                var isLast = i == lineSegments.Count - 1;
+
+                AddAppropriateStartEndCap(segments, lineSegment, isFirst);
+                AddAppropriateLine(segments, lineSegment);
+                AddAppropriateCorner(segments, lineSegment, isLast);
+                AddAppropriateFinishEndCap(segments, lineSegment, isLast);
+            }
+            return segments;
+        }
+
+        private void AddAppropriateStartEndCap(PathSegmentCollection segments, Tuple<Coords, Coords, Direction> lineSegment, bool isFirst)
+        {
+            if (!isFirst) return;
+            switch (lineSegment.Item3)
+            {
+                case Direction.Left:
+                    AddEndCapRight(segments, lineSegment.Item1);
+                    break;
+                case Direction.Right:
+                    AddEndCapLeft(segments, lineSegment.Item1);
+                    break;
+                case Direction.Up:
+                    AddEndCapBottom(segments, lineSegment.Item1);
+                    break;
+                case Direction.Down:
+                    AddEndCapTop(segments, lineSegment.Item1);
+                    break;
+                default:
+                    throw new InvalidOperationException("...");
+            }
+        }
+
+        private void AddAppropriateFinishEndCap(PathSegmentCollection segments, Tuple<Coords, Coords, Direction> lineSegment, bool isLast)
+        {
+            if (!isLast) return;
+            switch (lineSegment.Item3)
+            {
+                case Direction.Left:
+                    AddEndCapLeft(segments, lineSegment.Item2);
+                    break;
+                case Direction.Right:
+                    AddEndCapRight(segments, lineSegment.Item2);
+                    break;
+                case Direction.Up:
+                    AddEndCapTop(segments, lineSegment.Item2);
+                    break;
+                case Direction.Down:
+                    AddEndCapBottom(segments, lineSegment.Item2);
+                    break;
+                default:
+                    throw new InvalidOperationException("...");
+            }
+        }
+
+        private void AddAppropriateLine(PathSegmentCollection segments, Tuple<Coords, Coords, Direction> lineSegment)
+        {
+            switch (lineSegment.Item3)
+            {
+                case Direction.Left:
+                    AddLineHorizontal(segments, lineSegment.Item2, lineSegment.Item1);
+                    break;
+                case Direction.Right:
+                    AddLineHorizontal(segments, lineSegment.Item1, lineSegment.Item2);
+                    break;
+                case Direction.Up:
+                    AddLineVertical(segments, lineSegment.Item1, lineSegment.Item2);
+                    break;
+                case Direction.Down:
+                    AddLineVertical(segments, lineSegment.Item2, lineSegment.Item1);
+                    break;
+                default:
+                    throw new InvalidOperationException("...");
+            }
+        }
+
+        private void AddAppropriateCorner(PathSegmentCollection segments, Tuple<Coords, Coords, Direction> lineSegment, bool isLast)
+        {
         }
 
         private static PathSegmentCollection SortPathSegmentCollection(PathSegmentCollection segments)
         {
-            var sortedSegments = new List<PathSegment> {segments.First()};
             var n = segments.Count - 2;
-            Debug.Assert(n%2 == 0);
-            var m = n/2;
+            Debug.Assert(n % 2 == 0);
+            var m = n / 2;
+
+            // TODO: use better variables for v1, v2 and v3
             var v1 = Enumerable.Range(0, m).ToList();
             var v2 = v1.Select(x => x * 2 + 1);
             var v3 = v1.Select(x => x * 2 + 2);
+
+            var sortedSegments = new List<PathSegment> { segments.First() };
             sortedSegments.AddRange(v2.Select(x => segments[x]));
             sortedSegments.Add(segments.Last());
             sortedSegments.AddRange(v3.Select(x => segments[x]).Reverse());
+
             return new PathSegmentCollection(sortedSegments);
         }
 
-        private static CombinedGeometry CombineGeometries(IReadOnlyList<Geometry> geometries)
+        private static Geometry CombineGeometries(IReadOnlyList<Geometry> geometries)
         {
-            Debug.Assert(geometries.Count >= 2);
+            Debug.Assert(geometries.Count > 0);
+            if (geometries.Count == 1) return geometries.First();
             var seed = new CombinedGeometry(GeometryCombineMode.Union, geometries[0], geometries[1]);
             return geometries.Skip(2).Aggregate(seed, (cg, g) => new CombinedGeometry(GeometryCombineMode.Union, cg, g));
         }
@@ -347,58 +433,19 @@ namespace TetraSticks.View
 
         public void CombinedGeometryExperiment()
         {
-            var coords1 = new Coords(2, 2);
-            var coords2 = new Coords(3, 2);
+            var line1 = ImmutableList.Create(
+                new Coords(2, 2),
+                new Coords(3, 2));
 
-            var g1 = new PathGeometry();
-            var pf1 = new PathFigure
-            {
-                StartPoint = CoordsToInsetUpperLeftHorizontal(coords1)
-            };
-            AddEndCapLeft(pf1.Segments, coords1);
-            AddLineHorizontal(pf1.Segments, coords1, coords2);
-            //pf1.Segments.Add(new LineSegment
-            //{
-            //    Point = CoordsToInsetLowerRightHorizontal(coords2)
-            //});
-            AddEndCapRight(pf1.Segments, coords2);
-            //pf1.Segments.Add(new LineSegment
-            //{
-            //    Point = CoordsToInsetUpperLeftHorizontal(coords1)
-            //});
-            var sortedPf1Segments = SortPathSegmentCollection(pf1.Segments);
-            pf1.Segments = sortedPf1Segments;
-            g1.Figures.Add(pf1);
+            var line2 = ImmutableList.Create(
+                new Coords(0, 4),
+                new Coords(1, 4),
+                new Coords(2, 4),
+                new Coords(3, 4));
 
-            var coords3 = new Coords(0, 4);
-            var coords4 = new Coords(3, 4);
-
-            var g2 = new PathGeometry();
-            var pf2 = new PathFigure
-            {
-                StartPoint = CoordsToInsetUpperLeftHorizontal(coords3)
-            };
-            AddEndCapLeft(pf2.Segments, coords3);
-            pf2.Segments.Add(new LineSegment
-            {
-                Point = CoordsToInsetLowerRightHorizontal(coords4)
-            });
-            AddEndCapRight(pf2.Segments, coords4);
-            pf2.Segments.Add(new LineSegment
-            {
-                Point = CoordsToInsetUpperLeftHorizontal(coords3)
-            });
-            g2.Figures.Add(pf2);
-
-            var path = new Path
-            {
-                Data = CombineGeometries(ImmutableList.Create(g1, g2)),
-                StrokeThickness = 1,
-                Stroke = new SolidColorBrush(Colors.Black),
-                Fill = new SolidColorBrush(TetraStickColours.TetraStickToColour(Model.TetraSticks.V))
-            };
-
-            BoardCanvas.Children.Add(path);
+            var tempTetraStick = new TetraStick("V", ImmutableList<Coords>.Empty, line1, line2);
+            var tempPlacedTetraStick = new PlacedTetraStick(tempTetraStick, new Coords(0, 0), Orientation.North, ReflectionMode.Normal);
+            DrawPlacedTetraStick2(tempPlacedTetraStick);
         }
 
         private static IImmutableList<Point> PullLineEndsIn(IImmutableList<Point> transformedPts)
